2c7959c48caa869d625c6bd6278da4d46de5bc22
SnO2WMaN <me@sno2wman.net>
2023-01-02 17:51:06 +0900

    wip

---
+++ regex/src/engine/parser.rs
@@ -0,0 +1,66 @@
+use std::{
+    error::Error,
+    fmt::{self, Display},
+    mem::take,
+};
+
+#[derive(Debug)]
+pub enum AST {
+    Char(char),
+    Plus(Box<AST>),
+    Star(Box<AST>),
+    Question(Box<AST>),
+    Or(Box<AST>, Box<AST>),
+    Seq(Vec<AST>),
+}
+
+#[derive(Debug)]
+pub enum ParseError {
+    InvalidEscape(usize, char),
+    InvalidRightParen(usize),
+    NoPrev(usize),
+    NoRightParen,
+    Empty,
+}
+
+impl Error for ParseError {}
+
+fn parse_escape(pos: usize, c: char) -> Result<AST, ParseError> {
+    match c {
+        '(' | ')' | '|' | '+' | '*' | '?' => Ok(AST::Char(c)),
+        _ => Err(ParseError::InvalidEscape(pos, c)),
+    }
+}
+
+enum PSQ {
+    Plus,
+    Star,
+    Question,
+}
+
+fn parse_psq(seq: &mut Vec<AST>, ast_type: PSQ, pos: usize) -> Result<(), ParseError> {
+    if let Some(prev) = seq.pop() {
+        let ast = match ast_type {
+            PSQ::Plus => AST::Plus(Box::new(prev)),
+            PSQ::Star => AST::Star(Box::new(prev)),
+            PSQ::Question => AST::Question(Box::new(prev)),
+        };
+        seq.push(ast);
+        Ok(())
+    } else {
+        Err(ParseError::NoPrev(pos))
+    }
+}
+
+fn fold_or(mut seq_or: Vec<AST>) -> Option<AST> {
+    if seq_or.len() > 1 {
+        let mut ast = seq_or.pop().unwrap();
+        seq_or.reverse();
+        for s in seq_or {
+            ast = AST::Or(Box::new(s), Box::new(ast));
+        }
+        Some(ast)
+    } else {
+        seq_or.pop()
+    }
+}
